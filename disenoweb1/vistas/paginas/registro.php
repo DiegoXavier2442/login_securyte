0f3aa732e9971f35c0a179694bb4cafb0813933a
DiegoXavier2442 <diegoxavier20022020@gmail.com>
2025-11-01 23:08:51 -0500

    Pureba POTS

--- disenoweb1/vistas/paginas/registro.php
+++ disenoweb1/vistas/paginas/registro.php
@@ -1,21 +1,67 @@
-<h1>Registro</h1>
 
-<form >
+<div class="d-flex justify-content-center text-center"> 
+<form method="post">
   <div class="form-group">
-    <label for="email">Email address:</label>
-    <input type="email" class="form-control" id="email">
+    <label for="nombre">Nombre:</label>
+
+    <div class="input-group">
+  <div class="input-group-prepend">
+    <span class="input-group-text">
+      <i class="fas fa-user"></i>
+    </span>
+  </div>
+  <input type="text" class="form-control" id="nombre" name="registroNombre">
+</div>
+   
+  </div>
+
+  <div class="form-group">
+    <label for="email">Correo electrónico:</label>
+    
+    <div class="input-group">
+  <div class="input-group-prepend">
+    <span class="input-group-text">
+      <i class="fas fa-envelope"></i>
+    </span>
+    </div>
+    <input type="email" class="form-control" id="email"name="registroEmail">
+  </div>
+   
   </div>
 
   <div class="form-group">
     <label for="pwd">Password:</label>
-    <input type="password" class="form-control" id="pwd">
+    <div class="input-group">
+  <div class="input-group-prepend">
+    <span class="input-group-text">
+      <i class="fas fa-lock"></i>
+    </span>
+</div>
+    <input type="password" class="form-control" id="pwd"name="registroPassword">
   </div>
 
-  <div class="form-group form-check">
-    <label class="form-check-label">
-      <input class="form-check-input" type="checkbox"> Remember me
-    </label>
   </div>
+  <?php
 
-  <button type="submit" class="btn btn-primary">Submit</button>
+  //$registro = new controladorFormularios ();
+  
+ // $registro -> ctrRegistro();
+
+ $registro = controladorFormularios::ctrRegistro();
+ if($registro=="ok"){
+
+  
+
+  echo'<div class = "alert alert-success"> El usuario ha sido registrado </div>';
+  
+ }
+
+
+
+  ?>
+  
+
+  <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+
+</div>
