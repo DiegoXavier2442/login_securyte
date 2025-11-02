a90b3af806346943a8eab4120bfa44af062323b9
DiegoXavier2442 <diegoxavier20022020@gmail.com>
2025-11-02 13:09:55 -0500

    Leer Registros

--- disenoweb1/vistas/paginas/inicio.php
+++ disenoweb1/vistas/paginas/inicio.php
@@ -1,28 +1,41 @@
-<h1>Inicio</h1>
+<?php
+// objeto que le hace una peticion al controlador 
+$usuarios = ControladorFormularios::ctrSelecionarRegistros();
+
+?>
+
 
 <table class="table table-striped">
   <thead>
     <tr>
-      <th>Firstname</th>
-      <th>Lastname</th>
+      <th>#</th>
+      <th>Nombre</th>
       <th>Email</th>
+      <th>Fecha</th>
+      <th>Acciones</th>
     </tr>
   </thead>
   <tbody>
+    
+    <?php foreach ($usuarios as $key => $value ):
+      // $key = viene siendo el numero de indice  al que pertenece el valor del arry $usuarios
+      // se suma +1 por que los arry inician desde cero  ?>
     <tr>
-      <td>John</td>
-      <td>Doe</td>
-      <td>john@example.com</td>
+      <th><?php echo ($key+1);?></th>
+      <td><?php echo $value["usuario"];?></td>
+      <td><?php echo $value["email"];?></td>
+      <td><?php echo $value["fecha_registro"];?></td>
+      <td>
+        <div class="btn-group">
+          <button class="btn btn-warning">
+            <i class="fas fa-pencil-alt"></i>
+          </button>
+          <button class="btn btn-danger">
+            <i class="fas fa-trash-alt"></i>
+          </button>
+        </div>
+      </td>
     </tr>
-    <tr>
-      <td>Mary</td>
-      <td>Moe</td>
-      <td>mary@example.com</td>
-    </tr>
-    <tr>
-      <td>July</td>
-      <td>Dooley</td>
-      <td>july@example.com</td>
-    </tr>
+    <?php endforeach?>
   </tbody>
 </table>
