1c77902c28f0ae1ac3736b08dae25aeda6f2bbb3
DiegoXavier2442 <diegoxavier20022020@gmail.com>
2025-11-04 16:28:51 -0500

    .htaccess

--- disenoweb1/vistas/plantilla.php
+++ disenoweb1/vistas/plantilla.php
@@ -38,13 +38,13 @@ BOTONERA GET
            <?php if ($_GET["pagina"] == "registro"): ?>
 
             <li class="nav-item">
-               <a class="nav-link active" href="index.php?pagina=registro">Registro</a>
+               <a class="nav-link active" href="registro">Registro</a>
             </li>
 
             <?php else: ?>
 
              <li class="nav-item">
-                <a class="nav-link" href="index.php?pagina=registro">Registro</a>
+                <a class="nav-link" href="registro">Registro</a>
             </li>
 
             <?php endif ?>
@@ -53,13 +53,13 @@ BOTONERA GET
             <?php if ($_GET["pagina"] == "ingreso"): ?>
 
             <li class="nav-item">
-               <a class="nav-link active" href="index.php?pagina=ingreso">Ingreso</a>
+               <a class="nav-link active" href="ingreso">Ingreso</a>
             </li>
 
             <?php else: ?>
 
              <li class="nav-item">
-                <a class="nav-link" href="index.php?pagina=ingreso">Ingreso</a>
+                <a class="nav-link" href="ingreso">Ingreso</a>
             </li>
 
             <?php endif ?>
@@ -69,11 +69,11 @@ BOTONERA GET
            <?php if ($_GET["pagina"] == "inicio"): ?>
 
             <li class="nav-item">
-             <a class="nav-link active" href="index.php?pagina=inicio">Inicio</a>
+             <a class="nav-link active" href="inicio">Inicio</a>
             </li>
             <?php else: ?>
              <li class="nav-item">
-             <a class="nav-link" href="index.php?pagina=inicio">Inicio</a>
+             <a class="nav-link" href="inicio">Inicio</a>
             </li>
 
             <?php endif ?>
@@ -83,11 +83,11 @@ BOTONERA GET
            <?php if ($_GET["pagina"] == "salir"): ?>
 
             <li class="nav-item">
-             <a class="nav-link active" href="index.php?pagina=salir">Salir</a>
+             <a class="nav-link active" href="salir">Salir</a>
             </li>
             <?php else: ?>
              <li class="nav-item">
-             <a class="nav-link" href="index.php?pagina=salir">Salir</a>
+             <a class="nav-link" href="salir">Salir</a>
             </li>
 
 
@@ -98,16 +98,16 @@ BOTONERA GET
 
             <?php else: ?>
             <li class="nav-item">
-            <a class="nav-link active " href="index.php?pagina=registro">Registro</a>
+            <a class="nav-link active " href="registro">Registro</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="index.php?pagina=ingreso">Ingreso</a>
+            <a class="nav-link" href="ingreso">Ingreso</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link active" href="index.php?pagina=inicio">Inicio</a>
+            <a class="nav-link active" href="inicio">Inicio</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="index.php?pagina=salir">Salir</a>
+            <a class="nav-link" href="salir">Salir</a>
             </li>
 
 
